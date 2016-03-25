cb04f7493e89f3971e006eab26a05459faec6b1b
Preyanshu Kumar <preyansh@cs.ualberta.ca>
2016-03-25 17:37:03 -0600

    adding a bunch more stuff for group navigation, will actually work on that stuff tomorrow though

--- group_homepage.php
+++ group_homepage.php
@@ -1,6 +1,5 @@
 <?php 
 	session_start();
-	echo "Hello $first_name";
  ?>
 <html lang = "en">
 <head>
@@ -22,16 +21,14 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="http://consort.cs.ualberta.ca/~preyansh/image_gallery.html">Gallery</a></li>
-        <li><a href="#">My Pictures</a></li>
-        <li><a href="#">Upload Photo</a></li>
-        <li><a href="http://consort.cs.ualberta.ca/~preyansh/group_homepage.php">Groups</li>
+        <li><a href="http://consort.cs.ualberta.ca/~preyansh/image_gallery.html">My Groups</a></li>
+        <li><a href="http://consort.cs.ualberta.ca/~preyansh/group_signup.php">Create New Group</li>
       </ul>
       <form class="navbar-form navbar-left" role="search">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
+          <input type="text" class="form-control" placeholder="Search for Groups">
         </div>
-        <button type="submit" class="btn btn-default">Submit</button>
+        <button type="submit" class="btn btn-default">Search</button>
       </form>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
