59228b09eb1fc93884479d67a1d4e84c798f8ea4
garyscary <gary.dhil@gmail.com>
2016-03-27 20:33:40 -0600

    Start making it pretty

--- login_submit_final.php
+++ login_submit_final.php
@@ -3,23 +3,32 @@
 session_start();
 ?>
 <head>
-<title>Login</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<title>Login | Photoshare</title>
 </head>
 
 <body>
 <h2>Login Here</h2>
 <form action="PHPconnectionDB_final.php" method="post">
-<fieldset>
+<fieldset class="form-group">
 <p>
 <label for="username">Username</label>
-<input type="text" id="username" name="username" value="" maxlength="40" />
+<input type="text" class="form-control" id="username" name="username" value="" maxlength="40" placeholder="Enter username" />
 </p>
 <p>
 <label for="passwordd">Password</label>
-<input type="text" id="passwordd" name="passwordd" value="" maxlength="40" />
+<input type="text"class="form-control" id="passwordd" name="passwordd" value="" maxlength="40" placeholder="Password" />
 </p>
 <p>
-<input type="submit" value="Login" />
+<button type="submit" class="btn btn-primary">Login</button>
+<button type="submit" class="btn btn-default">Register</button>
 </p>
 </fieldset>
 </form>
