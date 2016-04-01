fdce5d89a597bfcddeb2f6fffc6f7bcff143092b
zhirunn <zhirunn@gmail.com>
2016-03-31 18:47:00 -0600

    display is dead as a doornail

--- getImage.php
+++ getImage.php
@@ -1,13 +1,12 @@
 <?php
-  $id = $_GET["id"];
-  //$type = $_GET['type'];
+  $usern = $_SESSION["username"];
   $type = 'photo';
   $conn = oci_connect('gd1', 'N1o2t3h4i5');
-  $query = "SELECT photo FROM images WHERE subject = :id";
+  $query = "SELECT photo FROM images WHERE owner_name = 'test'";
   
   $stid = oci_parse($conn, $query);
   
-  oci_bind_by_name($stid, ":id", $id);
+  //oci_bind_by_name($stid, ":id", $usern);
   
   
   oci_execute($stid);
@@ -15,6 +14,8 @@
   $row = oci_fetch_array($stid);
   $type = strtoupper($type);
   $result = $row[$type]->load();
+  
+  echo "Hello, world!";
   
   header("Content-type: image/jpeg");
   echo $result;
